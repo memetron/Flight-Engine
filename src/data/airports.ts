609ab477f67b9567864873751df2b720233bfdfe
Jonas Ellington <jonas.ellington@gmail.com>
2021-11-13 20:49:38 -0600

    Update airports.ts

--- src/data/airports.ts
+++ src/data/airports.ts
@@ -12,7 +12,7 @@ export const airports: Airport[] = [
   },
   {
     code: 'DFW',
-    city: 'Dallas-Fort Worth',
+    city: 'Dallas',
     timezone: 'America/Chicago',
     location: {
       latitude: 32.8998,
@@ -29,132 +29,6 @@ export const airports: Airport[] = [
     },
   },
   {
-    code: 'LAX',
-    city: 'Los Angeles',
-    timezone: 'America/Los_Angeles',
-    location: {
-      latitude: 33.9416,
-      longitude: 118.4085,
-    },
-  },
-  {
-    code: 'ORD',
-    city: 'Chicago',
-    timezone: 'America/Chicago',
-    location: {
-      latitude: 41.9742,
-      longitude: 87.9073,
-    },
-  },
-  {
-    code: 'GSO',
-    city: 'Greensboro',
-    timezone: 'America/New_York',
-    location: {
-      latitude: 36.0726,
-      longitude: -79.792,
-    },
-  },
-  {
-    code: 'ATL',
-    city: 'Atlanta',
-    timezone: 'America/New_York',
-    location: {
-      latitude: 33.6404,
-      longitude: -84.4198,
-    },
-  },
-  {
-    code: 'DEN',
-    city: 'Denver',
-    timezone: 'America/Phoenix',
-    location: {
-      latitude: 39.8493,
-      longitude: -104.6738,
-    },
-  },
-  {
-    code: 'CLT',
-    city: 'Charlotte',
-    timezone: 'America/New_York',
-    location: {
-      latitude: 35.2138,
-      longitude: -80.943,
-    },
-  },
-  {
-    code: 'LAS',
-    city: 'Chicago',
-    timezone: 'America/Chicago',
-    location: {
-      latitude: 36.08601,
-      longitude: -115.1539,
-    },
-  },
-  {
-    code: 'PHX',
-    city: 'Phoenix',
-    timezone: 'America/Phoenix',
-    location: {
-      latitude: 36.1043,
-      longitude: -79.935,
-    },
-  },
-  {
-    code: 'MCO',
-    city: 'Orlando',
-    timezone: 'America/New_York',
-    location: {
-      latitude: 28.4179,
-      longitude: -81.3041,
-    },
-  },
-  {
-    code: 'SEA',
-    city: 'Seattle',
-    timezone: 'America/Los_Angeles',
-    location: {
-      latitude: 47.4435,
-      longitude: -122.3016,
-    },
-  },
-  {
-    code: 'MIA',
-    city: 'Miami',
-    timezone: 'America/Chicago',
-    location: {
-      latitude: 25.7969,
-      longitude: -80.2762,
-    },
-  },
-  {
-    code: 'IAH',
-    city: 'Houston',
-    timezone: 'America/Chicago',
-    location: {
-      latitude: 29.9902,
-      longitude: -95.3368,
-    },
-  },
-  {
-    code: 'FLL',
-    city: 'Fort Lauderdale',
-    timezone: 'America/New_York',
-    location: {
-      latitude: 26.0742,
-      longitude: -80.1506,
-    },
-  },
-  {
-    code: 'BWI',
-    city: 'Baltimore',
-    timezone: 'America/New_York',
-    location: {
-      latitude: 39.1774,
-      longitude: -76.6684,
-    },
-  },
-  {
     code: 'SFO',
     city: 'San Francisco',
     timezone: 'America/Los_Angeles',
@@ -164,75 +38,39 @@ export const airports: Airport[] = [
     },
   },
   {
-    code: 'EWR',
-    city: 'Newark',
-    timezone: 'America/New_York',
+    code: 'LHR',
+    city: 'London',
+    timezone: 'Europe/Greenwich',
     location: {
-      latitude: 40.6895,
-      longitude: -74.1745,
+      latitude: 51.5072,
+      longitude: 0.1276,
     },
   },
   {
-    code: 'MSP',
-    city: 'Minneapolis',
-    timezone: 'America/Chicago',
+    code: 'CDG',
+    city: 'Paris',
+    timezone: 'Europe/Central',
     location: {
-      latitude: 44.8848,
-      longitude: -93.2223,
+      latitude: 48.8566,
+      longitude: 2.3522,
     },
   },
   {
-    code: 'DTW',
-    city: 'Detroit',
-    timezone: 'America/New_York',
+    code: 'SXF',
+    city: 'Berlin',
+    timezone: 'Europe/Central',
     location: {
-      latitude: 36.1043,
-      longitude: -79.935,
+      latitude: 52.5200,
+      longitude: 13.4050,
     },
   },
   {
-    code: 'BOS',
-    city: 'Chicago',
-    timezone: 'America/Chicago',
+    code: 'BLR',
+    city: 'Bangalore',
+    timezone: 'Europe/Central',
     location: {
-      latitude: 42.3656,
-      longitude: -71.0096,
+      latitude: 12.9716,
+      longitude: 77.5946,
     },
-  },
-  {
-    code: 'TPA',
-    city: 'Tampa',
-    timezone: 'America/New_York',
-    location: {
-      latitude: 36.1043,
-      longitude: -79.935,
-    },
-  },
-  {
-    code: 'SLC',
-    city: 'Salt Lake City',
-    timezone: 'America/Chicago',
-    location: {
-      latitude: 40.7899,
-      longitude: -111.9791,
-    },
-  },
-  {
-    code: 'SAN',
-    city: 'San Diego',
-    timezone: 'America/Los_Angeles',
-    location: {
-      latitude: 32.7338,
-      longitude: -117.1933,
-    },
-  },
-  {
-    code: 'PHL',
-    city: 'Philadelphia',
-    timezone: 'America/New_York',
-    location: {
-      latitude: 39.8729,
-      longitude: -75.2437,
-    },
-  },
+  }
 ];
